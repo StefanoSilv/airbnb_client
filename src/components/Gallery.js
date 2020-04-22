e80d76f2153a58b26865a3c9609cb6db2f24bab9
StefanoSilv <silver.stefano@gmail.com>
2020-04-22 11:51:37 +0200

    component for a gallery created

---
+++ src/components/Gallery.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Gallery extends Component {
+	state = {
+		images: [],
+		mainImage: ''
+	};
+	componentWillReceiveProps(props) {
+		if (typeof props.images !== 'undefined') {
+			this.setState({ images: props.images, mainImage: props.images[0] });
+		}
+	}
+	selectImage = idx => {
+		this.setState({ mainImage: this.state.images[idx] });
+	};
+	render() {
+		return (
+			<div className="gallery">
+				<div
+					className="image-main"
+					style={{ backgroundImage: `url(${this.state.mainImage})` }}
+				></div>
+				<div className="previews">
+					{this.state.images.map((image, i) => (
+						<div
+							className="preview"
+							key={i}
+							style={{ backgroundImage: `url(${image})` }}
+							onClick={() => this.selectImage(i)}
+						></div>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Gallery;
